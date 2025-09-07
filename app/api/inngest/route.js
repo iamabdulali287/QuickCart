5e1d4247d3bd4f70f72a5cc0a0576801f2c16efc
iamabdulali287 <iamabdulali287@gmail.com>
2025-09-07 21:24:38 +0500

    fix eslint built issue

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -1,4 +1,4 @@
-import { serve } from "inngest/next";
+import { serve } from "@/config/inngest";
 import { inngest, syncUserCreation, syncUserDeletion, syncUserUpdation } from "@/config/inngest";
 
 // Create an API that serves zero functions
--- config/inngest.js
+++ config/inngest.js
@@ -25,7 +25,7 @@ export const syncUserCreation = inngest.createFunction(
 );
 //  function to update userdata in database
 
-export const syncUserUpdation = innegest.createFunction(
+export const syncUserUpdation = inngest.createFunction(
   {
     id: "update-user-from-clerk",
   },
@@ -36,7 +36,7 @@ export const syncUserUpdation = innegest.createFunction(
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
     const userData = {
-      _Id: id,
+      _id: id,
       email: email_addresses[0].email_address,
       name: first_name + " " + last_name,
       imageUrl: image_url,
