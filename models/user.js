1077993809db06ffa83a5408ac5f106e480360b0
iamabdulali287 <iamabdulali287@gmail.com>
2025-09-07 19:47:33 +0500

    First Commit

---
+++ models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    _Id: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, default: {} },
+  },
+  { minimize: false }
+);
+const User = mongoose.models.user || mongoose.model("user", userSchema);
+
+export default User;
